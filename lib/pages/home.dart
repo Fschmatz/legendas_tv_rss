7482d3c7c3cbb3e6a8ed732ed801ed8b3342088a
Fernando <fschmatz@hotmail.com>
2021-12-19 12:17:16 -0300

    v 1.3.0

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -137,54 +137,60 @@ class _HomeState extends State<Home> {
               ),
       ),
       bottomNavigationBar:
-      NavigationBar(
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        selectedIndex: _currentIndex,
-        onDestinationSelected: (index) {
-          setState(() {
-            loading = true;
-            _currentIndex = index;
-            if (index == 0) {
-              feedSelecionado = destaques;
-            } else if (index == 1) {
-              feedSelecionado = filmes;
-            } else if (index == 2) {
-              feedSelecionado = series;
-            } else if (index == 3) {
-              feedSelecionado = cartoons;
-            } else if (index == 4) {
-              feedSelecionado = novidades;
-            }
-          });
-          getRssData(feedSelecionado);
-        },
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.priority_high_outlined),
-            selectedIcon: Icon(Icons.priority_high,color: Colors.black87,),
-            label: 'Destaques',
+      Container(
+        color: Theme.of(context).navigationBarTheme.backgroundColor,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: NavigationBar(
+            labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            selectedIndex: _currentIndex,
+            onDestinationSelected: (index) {
+              setState(() {
+                loading = true;
+                _currentIndex = index;
+                if (index == 0) {
+                  feedSelecionado = destaques;
+                } else if (index == 1) {
+                  feedSelecionado = filmes;
+                } else if (index == 2) {
+                  feedSelecionado = series;
+                } else if (index == 3) {
+                  feedSelecionado = cartoons;
+                } else if (index == 4) {
+                  feedSelecionado = novidades;
+                }
+              });
+              getRssData(feedSelecionado);
+            },
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.priority_high_outlined),
+                selectedIcon: Icon(Icons.priority_high,color: Colors.black87,),
+                label: 'Destaques',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.movie_creation_outlined),
+                selectedIcon: Icon(Icons.movie_creation,color: Colors.black87,),
+                label: 'Filmes',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.tv_outlined),
+                selectedIcon: Icon(Icons.tv,color: Colors.black87,),
+                label: 'Séries',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.brush_outlined),
+                selectedIcon: Icon(Icons.brush,color: Colors.black87,),
+                label: 'Cartoons',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.watch_later_outlined),
+                selectedIcon: Icon(Icons.watch_later,color: Colors.black87,),
+                label: 'Novidades',
+              ),
+            ],
           ),
-          NavigationDestination(
-            icon: Icon(Icons.movie_creation_outlined),
-            selectedIcon: Icon(Icons.movie_creation,color: Colors.black87,),
-            label: 'Filmes',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.tv_outlined),
-            selectedIcon: Icon(Icons.tv,color: Colors.black87,),
-            label: 'Séries',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.brush_outlined),
-            selectedIcon: Icon(Icons.brush,color: Colors.black87,),
-            label: 'Cartoons',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.watch_later_outlined),
-            selectedIcon: Icon(Icons.watch_later,color: Colors.black87,),
-            label: 'Novidades',
-          ),
-        ],
+        ),
       ),
     );
   }
